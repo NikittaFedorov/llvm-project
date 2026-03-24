8ccfde39b44e179998e4bc0e1ea0f7c8f0e02884
Nikita Fedorov <lednikita0481@gmail.com>
2026-03-25 00:41:55 +0300

    [hw2] 15. Extend YASarchTargetLowering with YASarchRegisterInfo

--- llvm/lib/Target/YASarch/YASarchISelLowering.cpp
+++ llvm/lib/Target/YASarch/YASarchISelLowering.cpp
@@ -39,6 +39,28 @@ YASarchTargetLowering::YASarchTargetLowering(const TargetMachine &TM,
     : TargetLowering(TM), STI(STI) {
   YASarch_DUMP_RED
   addRegisterClass(MVT::i32, &YASarch::GPRRegClass);
+
+  computeRegisterProperties(STI.getRegisterInfo());
+
+  setStackPointerRegisterToSaveRestore(YASarch::R1);
+
+  // setSchedulingPreference(Sched::Source);
+
+  for (unsigned Opc = 0; Opc < ISD::BUILTIN_OP_END; ++Opc)
+    setOperationAction(Opc, MVT::i32, Expand);
+
+  setOperationAction(ISD::ADD, MVT::i32, Legal);
+  setOperationAction(ISD::MUL, MVT::i32, Legal);
+  // ...
+  setOperationAction(ISD::LOAD, MVT::i32, Legal);
+  setOperationAction(ISD::STORE, MVT::i32, Legal);
+
+  setOperationAction(ISD::Constant, MVT::i32, Legal);
+  setOperationAction(ISD::UNDEF, MVT::i32, Legal);
+
+  setOperationAction(ISD::BR_CC, MVT::i32, Custom);
+
+  setOperationAction(ISD::FRAMEADDR, MVT::i32, Legal);
 }
 
 const char *YASarchTargetLowering::getTargetNodeName(unsigned Opcode) const {
