1675cdd0fd90976197ebb280b81d974f6def2ab0
Nikita Fedorov <lednikita0481@gmail.com>
2026-04-07 22:53:40 +0300

    [hw3] 21. Add YASarchELFObjectWriter

--- llvm/lib/Target/YASarch/MCTargetDesc/YASarchMCTargetDesc.h
+++ llvm/lib/Target/YASarch/MCTargetDesc/YASarchMCTargetDesc.h
@@ -1,10 +1,13 @@
 #pragma once
 
+#include <memory>
+
 namespace llvm {
 class MCCodeEmitter;
 class MCContext;
 class MCInstrInfo;
 class MCAsmBackend;
+class MCObjectTargetWriter;
 class MCRegisterInfo;
 class MCSubtargetInfo;
 class MCTargetOptions;
@@ -16,6 +19,8 @@ MCAsmBackend *createYASarchAsmBackend(const Target &T,
                                       const MCSubtargetInfo &STI,
                                       const MCRegisterInfo &MRI,
                                       const MCTargetOptions &Options);
+std::unique_ptr<MCObjectTargetWriter>
+createYASarchELFObjectWriter(bool Is64Bit, uint8_t OSABI);
 } // namespace llvm
 
 // Defines symbolic names for YASarch registers.  This defines a mapping from
